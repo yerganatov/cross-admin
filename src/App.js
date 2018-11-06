91ce877687c32141032d5f11bf1e3594846c79ec
hrmnt <cjkhormonot@gmail.com>
2018-11-06 06:09:27 +0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import "./bootstrap.css";
 import './App.css';
 import {db} from "./firebase";
 
@@ -56,26 +57,40 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <h1>ru</h1>
-                <input onChange={ (event) => this.changeValue("ru","title",event)} placeholder="title" value={this.state.ru.title} type="text"/>
-                <input onChange={ (event) => this.changeValue("ru","done",event)} placeholder="done" value={this.state.ru.done} type="text"/>
-                <input onChange={ (event) => this.changeValue("ru","goal",event)} placeholder="goal" value={this.state.ru.goal} type="text"/>
-                <input onChange={ (event) => this.changeValue("ru","aboutClient",event)} placeholder="aboutClient" value={this.state.ru.aboutClient} type="text"/>
-                <input onChange={ (event) => this.changeValue("ru","result",event)} placeholder="result" value={this.state.ru.result} type="text"/>
-                <h1>en</h1>
-                <input onChange={ (event) => this.changeValue("en","title",event)} placeholder="title" value={this.state.en.title} type="text"/>
-                <input onChange={ (event) => this.changeValue("en","done",event)} placeholder="done" value={this.state.en.done} type="text"/>
-                <input onChange={ (event) => this.changeValue("en","goal",event)} placeholder="goal" value={this.state.en.goal} type="text"/>
-                <input onChange={ (event) => this.changeValue("en","aboutClient",event)} placeholder="aboutClient" value={this.state.en.aboutClient} type="text"/>
-                <input onChange={ (event) => this.changeValue("en","result",event)} placeholder="result" value={this.state.en.result} type="text"/>
-                <h1>gr</h1>
-                <input onChange={ (event) => this.changeValue("gr","title",event)} placeholder="title" value={this.state.gr.title} type="text"/>
-                <input onChange={ (event) => this.changeValue("gr","done",event)} placeholder="done" value={this.state.gr.done} type="text"/>
-                <input onChange={ (event) => this.changeValue("gr","goal",event)} placeholder="goal" value={this.state.gr.goal} type="text"/>
-                <input onChange={ (event) => this.changeValue("gr","aboutClient",event)} placeholder="aboutClient" value={this.state.gr.aboutClient} type="text"/>
-                <input onChange={ (event) => this.changeValue("gr","result",event)} placeholder="result" value={this.state.gr.result} type="text"/>
-
+            <div className="App d-flex">
+                <div className="sidebar d-flex flex-column py-4">
+                    <a className="d-flex justify-content-center align-self-center mb-4" href=""><img src={process.env.PUBLIC_URL + 'logo-mobile.png'} alt=""/></a>
+                    <a href="">Добавление каталога</a>
+                    <a href="">Список каталогов</a>
+                    <a href="">Добавление проекта</a>
+                    <a href="">Список проектов</a>
+                    <a href="">Выйти</a>
+                </div>
+                <div className="main-content p-5 d-flex flex-column">
+                    <h1>Добавление нового каталога</h1>
+                    <p>Все поля обязательны к заполнению</p>
+                    <form className="d-flex flex-column h-100" action="">
+                        <h3>Информация на русском языке</h3>
+                        <input onChange={ (event) => this.changeValue("ru","title",event)} placeholder="title" value={this.state.ru.title} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("ru","done",event)} placeholder="done" value={this.state.ru.done} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("ru","goal",event)} placeholder="goal" value={this.state.ru.goal} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("ru","aboutClient",event)} placeholder="aboutClient" value={this.state.ru.aboutClient} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("ru","result",event)} placeholder="result" value={this.state.ru.result} type="text" required/>
+                        <h3>Информация на английском языке</h3>
+                        <input onChange={ (event) => this.changeValue("en","title",event)} placeholder="title" value={this.state.en.title} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("en","done",event)} placeholder="done" value={this.state.en.done} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("en","goal",event)} placeholder="goal" value={this.state.en.goal} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("en","aboutClient",event)} placeholder="aboutClient" value={this.state.en.aboutClient} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("en","result",event)} placeholder="result" value={this.state.en.result} type="text" required/>
+                        <h3>Информация на немецком языке</h3>
+                        <input onChange={ (event) => this.changeValue("gr","title",event)} placeholder="title" value={this.state.gr.title} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("gr","done",event)} placeholder="done" value={this.state.gr.done} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("gr","goal",event)} placeholder="goal" value={this.state.gr.goal} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("gr","aboutClient",event)} placeholder="aboutClient" value={this.state.gr.aboutClient} type="text" required/>
+                        <input onChange={ (event) => this.changeValue("gr","result",event)} placeholder="result" value={this.state.gr.result} type="text" required/>
+                        <button type="button" class="btn btn-primary">Добавить</button>
+                    </form>
+                </div>
             </div>
         );
     }
